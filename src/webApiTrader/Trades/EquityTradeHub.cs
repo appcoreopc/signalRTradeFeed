7cbe12d115763c9cc67cea4ce60f734fdc4fc989
PELCORP\X1100543 <jeremy.woowengkeong@countdown.co.nz>
2021-05-25 09:36:25 +1200

    migrated signalR into a webapi.

---
+++ src/webApiTrader/Trades/EquityTradeHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace webApiTrader.Trades
+{
+    public class EquityTradeHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("receivemessage", user, message);
+        }
+    }
+}
