44c8ffd6ecbefe934512ea70c4068c5a3004a9ac
PELCORP\X1100543 <jeremy.woowengkeong@countdown.co.nz>
2021-06-03 08:56:27 +1200

    setting up the ability to post / queue a build.

--- src/AzureDevOps.Integration/Clients/HttpRestClient.cs
+++ src/AzureDevOps.Integration/Clients/HttpRestClient.cs
@@ -8,6 +8,7 @@ namespace AzureDevOps.Integration.REST.Clients
     public class HttpRestClient
     {
         private const string JsonMediaType = "application/json";
+        private const string AuthenticationHeaderMethod = "Basic";
 
         private readonly string _personalaccesstoken;
 
@@ -15,14 +16,14 @@ namespace AzureDevOps.Integration.REST.Clients
         {
             _personalaccesstoken = patToken;
         }
-        public async Task Execute(string targetUrl)
+        public async Task ExecuteGetAsync(string targetUrl)
         {
             using (HttpClient client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Accept.Add(
                     new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(JsonMediaType));
 
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(AuthenticationHeaderMethod,
                     Convert.ToBase64String(
                         System.Text.ASCIIEncoding.ASCII.GetBytes(
                             string.Format("{0}:{1}", "", _personalaccesstoken))));
@@ -35,6 +36,24 @@ namespace AzureDevOps.Integration.REST.Clients
                 }
             }
         }
+
+        public async Task ExecutePostAsync(string targetUrl, string payload)
+        {          
+            StringContent content = new StringContent(payload, System.Text.Encoding.UTF8, JsonMediaType);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(JsonMediaType));
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(AuthenticationHeaderMethod, Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", _personalaccesstoken))));
+                              
+                using (HttpResponseMessage response = await client.PostAsync(targetUrl, content))
+                {
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+            }
+        }
     }
 }
-
--- src/ClientApiTest/Program.cs
+++ src/ClientApiTest/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AzureDevOps.Integration.REST.Clients;
-using Flurl.Http;
+using Newtonsoft.Json;
 
 namespace ClientApiTest
 {
@@ -11,10 +11,40 @@ namespace ClientApiTest
         {
             Console.WriteLine("Hello World!");
 
-            var c = new HttpRestClient("");
+            var c = new HttpRestClient("g5ia7mwddvuf22bslp6lxfqs5mqx4b5gb7sfxd2q65odgngcvkjq");
+
             //await c.Execute("https://dev.azure.com/wwnz/API/_apis/build/builds?api-version=6.0");
 
-            await c.Execute("https://dev.azure.com/WWNZ/API/_apis/pipelines/677/runs/76870?api-version=6.0-preview.1");
+            //await c.ExecuteGetAsync("https://dev.azure.com/WWNZ/API/_apis/pipelines/677/runs/76870?api-version=6.0-preview.1");
+
+            // queue a build 
+            // https://dev.azure.com/{organization}/{project}/_apis/build/builds?api-version=6.0
+
+            var newBuild = new RunPipeline
+            {
+                Organization = "WWNZ",
+                Project = "API", 
+                APIVersion = "6.0",               
+                PipelineId = 677, 
+                PipelineVersion = 6
+            };
+
+            var json = JsonConvert.SerializeObject(newBuild);
+            await c.ExecutePostAsync($"https://dev.azure.com/{newBuild.Organization}/{newBuild.Project}/_apis/pipelines/{newBuild.PipelineId}/runs?api-version=6.0-preview.1", json);
         }
+    }
+
+    class RunPipeline
+    {
+        public string Organization { get; set; }
+        public int PipelineId { get; set; }
+
+        public string Project { get; set; }
+
+        [JsonProperty("api-version")]
+        public string APIVersion { get; set; }
+
+        public int PipelineVersion { get; set; }
+            
     }
 }
