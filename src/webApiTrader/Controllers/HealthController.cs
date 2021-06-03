b4c9d2541ce698bb2ab4b6d5a092ea217fda3090
PELCORP\X1100543 <jeremy.woowengkeong@countdown.co.nz>
2021-06-04 08:18:23 +1200

    added health check controller.

---
+++ src/webApiTrader/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace webApiTrader.Controllers
+{
+    public class HealthController : ControllerBase
+    {       
+        [HttpGet]
+        public async Task<IActionResult> Ping()
+        {
+            await Task.Delay(1000);
+            return Ok(DateTime.Now);
+        }    
+    }
+}
